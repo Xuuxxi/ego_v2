d8e3fdc5fd2c6089aa6aaffe611bd6384e2739a3
Xuuxxi <2933171792@qq.com>
2022-05-05 20:27:21 +0800

    解决 Js 和 Java Long精度问题

---
+++ src/main/java/com/handsUp/ego_v2/config/WebMvcConfig.java
@@ -0,0 +1,35 @@
+package com.handsUp.ego_v2.config;
+
+
+import com.handsUp.ego_v2.common.JacksonObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * @Author: Xuuxxi
+ * @Date: 2022/5/3
+ */
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 解决Long精度转化问题
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("消息转化器启动...");
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new JacksonObjectMapper());
+
+        converters.add(0,converter);
+    }
+}
