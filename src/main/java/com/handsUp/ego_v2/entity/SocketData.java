15dd214bf2abd522da567574cba7f2757af49596
LiuJunhui <1436216510@qq.com>
2022-05-26 13:31:09 +0800

    对websocket注入的修正

--- src/main/java/com/handsUp/ego_v2/entity/SocketData.java
+++ src/main/java/com/handsUp/ego_v2/entity/SocketData.java
@@ -1,5 +1,6 @@
 package com.handsUp.ego_v2.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -15,14 +16,23 @@ import java.time.LocalDateTime;
 @TableName("socket_data")
 public class SocketData implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @TableField("from")
     private Long from;
 
+    @TableField("to")
     private Long to;
 
+    @TableField("send_time")
     private LocalDateTime sendTime;
 
+    @TableField("msg")
+    private String msg;
+
+    @TableField("is_read")
     private Integer isRead;
 
-    private String msg;
+
 
 }
--- src/test/java/com/handsUp/ego_v2/EgoV2ApplicationTests.java
+++ src/test/java/com/handsUp/ego_v2/EgoV2ApplicationTests.java
@@ -1,35 +1,49 @@
 package com.handsUp.ego_v2;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.handsUp.ego_v2.entity.SocketData;
 import com.handsUp.ego_v2.entity.User;
+import com.handsUp.ego_v2.mapper.SocketMapper;
+import com.handsUp.ego_v2.service.SocketService;
 import com.handsUp.ego_v2.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootTest
+import java.time.LocalDateTime;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
+@EnableAsync
+@ServletComponentScan
+@EnableTransactionManagement
 class EgoV2ApplicationTests {
+
     @Autowired
-    UserService userService;
+    SocketMapper socketMapper;
+
+    @Autowired
+    SocketService socketService;
+
 
     @Test
-    void contextLoads() {
+    void test(){
 
-        User user = new User();
-        user.setUserNickName("xiaoMing");
-        user.setUserName("test1");
-        user.setPassword("123456");
-        user.setIsBanned(0);
+        SocketData socketData = new SocketData();
+        socketData.setFrom(123L);
+        socketData.setTo(456L);
+        socketData.setSendTime(LocalDateTime.now());
+        socketData.setMsg("hello");
+        socketData.setIsRead(1);
 
-        userService.save(user);
+        socketMapper.insert(socketData);
 
-        //service可用
     }
-
-    @Test
-    void logTest(){
-        log.info(null);
-    }
-
 }
