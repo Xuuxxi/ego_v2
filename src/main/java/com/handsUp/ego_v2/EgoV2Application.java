09b16362891905523a0fcc4b6f747bc5f8f36d92
LiuJunhui <1436216510@qq.com>
2022-05-30 18:08:24 +0800

    最后的修正

--- src/main/java/com/handsUp/ego_v2/EgoV2Application.java
+++ src/main/java/com/handsUp/ego_v2/EgoV2Application.java
@@ -3,17 +3,17 @@ package com.handsUp.ego_v2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableWebSecurity
+
 @EnableAsync
 @Slf4j
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
 public class EgoV2Application {
