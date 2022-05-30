0577fb6b0ef2f4f3c8094f81c177487ecc98c755
LiuJunhui <1436216510@qq.com>
2022-05-30 15:27:46 +0800

    最后的修正

--- src/main/java/com/handsUp/ego_v2/config/SecurityConfig.java
+++ src/main/java/com/handsUp/ego_v2/config/SecurityConfig.java
@@ -15,6 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        return;
     }
 }
